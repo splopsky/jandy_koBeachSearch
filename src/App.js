d48d6997b1731b48b55296c5e6934b577b93f766
splopsky <hebin810@naver.com>
2021-05-09 00:46:07 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import JSONDATA from './example.json';
+import {useState} from 'react';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <header>
+    <h1>🏖 해변으로 가요 🏝</h1>
+  </header>
+  <div className="App">
+    <div className="search">
+    <input type="text" placeholder="검색어를 입력하세요..." onChange={event => {setSearchTerm(event.target.value)}}/>
     </div>
+    <div className="container">
+    {JSONDATA.filter((val)=> {
+      if (searchTerm == "")
+      {return val}
+      else if (val.sta_nm.toLowerCase().includes(searchTerm.toLowerCase()) || val.sido_nm.toLowerCase().includes(searchTerm.toLowerCase()))
+      {return val}
+    }).map((val, key)=> {
+      return <div className="beaches" key={key}>
+        <img src={val.beach_img}/>
+        <h5>{val.sta_nm}</h5>
+        </div>
+    })}
+    </div>
+  </div>
+  </div>
   );
 }
+
+
+
 
 export default App;
